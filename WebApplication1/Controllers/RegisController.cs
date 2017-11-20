00f4aecbe78ffc0f1ad141c4aee9caf9699b6a41
SupanunJ <32703392+SupanunJ@users.noreply.github.com>
2017-11-21 00:25:26 +0700

    register

--- WebApplication1/Controllers/RegisController.cs
+++ WebApplication1/Controllers/RegisController.cs
@@ -7,15 +7,45 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    [Route("[controller]/[action]")]
     public class RegisController : Controller
     {
         //DB connection
         private linebotDBEntities db = new linebotDBEntities();
         //
         // GET: Regis
-        public ActionResult Index()
+        [Route("{id}")]
+        public ActionResult Index(string id)
         {
+            ViewBag.idreturn = id;
             return View();
+        }
+        
+
+        [HttpPost]
+        public ActionResult LineRegister()
+        {
+            var c = new customer
+            {
+                line_id = Request.Form["u_id"],
+                u_name = Request.Form["name"],
+                u_lastname = Request.Form["lastname"],
+                u_status = 1,
+                u_tel = Request.Form["tel"],
+                house_no = Request.Form["house_no"],
+                village = Request.Form["village"],
+                lane = Request.Form["lane"],
+                road = Request.Form["road"],
+                subarea = Request.Form["subarea"],
+                area = Request.Form["area"],
+                province = Request.Form["province"],
+                postal_code = Request.Form["postal_code"],
+                annotation = Request.Form["annotation"]
+            };
+
+            db.customers.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
